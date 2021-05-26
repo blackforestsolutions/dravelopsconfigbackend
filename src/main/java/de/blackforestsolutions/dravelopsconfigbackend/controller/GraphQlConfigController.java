0dd0cd496f5d127c06f0a388e7a2017faefd82c3
Luca1235 <luca.we98@gmx.de>
2021-05-26 11:55:19 +0200

    Started Controller Implementation #minor

--- src/main/java/de/blackforestsolutions/dravelopsconfigbackend/controller/GraphQlConfigController.java
+++ src/main/java/de/blackforestsolutions/dravelopsconfigbackend/controller/GraphQlConfigController.java
@@ -1,5 +1,12 @@
 package de.blackforestsolutions.dravelopsconfigbackend.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import de.blackforestsolutions.dravelopsconfigbackend.service.communicationservice.GitHubApiService;
+import de.blackforestsolutions.dravelopsdatamodel.CallStatus;
+import de.blackforestsolutions.dravelopsgeneratedcontent.graphql.GraphQLApiConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("configbackend")
 public class GraphQlConfigController {
 
+    private final GitHubApiService apiService;
+
+    @Autowired
+    public GraphQlConfigController(GitHubApiService apiService) {
+        this.apiService = apiService;
+    }
+
+    @GetMapping
+    public CallStatus<GraphQLApiConfig> get() {
+        return apiService.getGraphQlApiConfig();
+    }
+
+    @PutMapping
+    public CallStatus<String> put(GraphQLApiConfig apiConfig) throws JsonProcessingException {
+        return apiService.putGraphQlApiConfig(apiConfig);
+    }
 }
