5c584fff0f862dfb06606603153f7b474db4af98
Luca1235 <luca.we98@gmx.de>
2021-05-31 09:56:36 +0200

    Added RequestBody Annotation to putGraphQlApiConfig in GraphQlConfigController.java to receive GraphQLApiConfig from configfrontend #patch

--- src/main/java/de/blackforestsolutions/dravelopsconfigbackend/controller/GraphQlConfigController.java
+++ src/main/java/de/blackforestsolutions/dravelopsconfigbackend/controller/GraphQlConfigController.java
@@ -6,10 +6,7 @@ import de.blackforestsolutions.dravelopsdatamodel.GraphQlTab;
 import de.blackforestsolutions.dravelopsgeneratedcontent.graphql.GraphQLApiConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,7 +27,7 @@ public class GraphQlConfigController {
     }
 
     @PutMapping
-    public ResponseEntity<List<CallStatus<GraphQlTab>>> putGraphQlApiConfig(GraphQLApiConfig apiConfig) {
+    public ResponseEntity<List<CallStatus<GraphQlTab>>> putGraphQlApiConfig(@RequestBody GraphQLApiConfig apiConfig) {
         return configIntegrationService.putGraphQlApiConfig(apiConfig);
     }
 }
