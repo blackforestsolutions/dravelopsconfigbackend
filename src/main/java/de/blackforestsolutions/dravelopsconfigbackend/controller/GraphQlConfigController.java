5df171c7c3d7efbb482550a682aec6d2498ba365
Luca1235 <luca.we98@gmx.de>
2021-06-01 12:45:42 +0200

    Fixed Cors Issue #patch

--- src/main/java/de/blackforestsolutions/dravelopsconfigbackend/controller/GraphQlConfigController.java
+++ src/main/java/de/blackforestsolutions/dravelopsconfigbackend/controller/GraphQlConfigController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:8089")
 @RestController
 @RequestMapping("configbackend")
 public class GraphQlConfigController {
