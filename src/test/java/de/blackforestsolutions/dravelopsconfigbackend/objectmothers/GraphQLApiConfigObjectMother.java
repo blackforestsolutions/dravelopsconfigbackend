cefeb751a484b178c0c38bd05d5d4e3e99ca077c
Luca1235 <luca.we98@gmx.de>
2021-06-10 15:42:52 +0200

    Adjusted Mapper in order to only send info for JourneyQuery and not JourneySubscription. Added logic to map changes of JourneyQuery to JourneySubscription #minor

--- src/test/java/de/blackforestsolutions/dravelopsconfigbackend/objectmothers/GraphQLApiConfigObjectMother.java
+++ src/test/java/de/blackforestsolutions/dravelopsconfigbackend/objectmothers/GraphQLApiConfigObjectMother.java
@@ -1,16 +1,32 @@
 package de.blackforestsolutions.dravelopsconfigbackend.objectmothers;
 
 import de.blackforestsolutions.dravelopsgeneratedcontent.graphql.GraphQLApiConfig;
+import de.blackforestsolutions.dravelopsgeneratedcontent.graphql.Tab;
 
 import static de.blackforestsolutions.dravelopsconfigbackend.objectmothers.GraphQlObjectMother.getGraphQlWithNoEmptyFields;
+import static de.blackforestsolutions.dravelopsdatamodel.GraphQlTab.JOURNEY_SUBSCRIPTION;
 
 public class GraphQLApiConfigObjectMother {
+
+    private static final String SHA = "466c1149863ba873988b38a0aed477bfce968e7c";
 
     public static GraphQLApiConfig getGraphQLApiConfigWithNoEmptyFields() {
         GraphQLApiConfig config = new GraphQLApiConfig();
 
         config.setGraphql(getGraphQlWithNoEmptyFields());
-        config.setSha("466c1149863ba873988b38a0aed477bfce968e7c");
+        config.setSha(SHA);
+
+        return config;
+    }
+
+    public static GraphQLApiConfig getGraphQLApiConfigWithMissingJourneySubscriptionTabExceptName() {
+        GraphQLApiConfig config = new GraphQLApiConfig();
+
+        config.setGraphql(getGraphQlWithNoEmptyFields());
+        Tab tab = new Tab();
+        tab.setName(config.getGraphql().getPlayground().getTabs().get(JOURNEY_SUBSCRIPTION.toString()).getName());
+        config.getGraphql().getPlayground().getTabs().put(JOURNEY_SUBSCRIPTION.toString(), tab);
+        config.setSha(SHA);
 
         return config;
     }
